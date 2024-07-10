5a6c04fe880a1468167ccad40d3e352b9fa7de17
wangwb <838524022@qq.com>
2024-07-10 20:46:00 +0800

    1

--- excel_module.py
+++ excel_module.py
@@ -31,18 +31,40 @@ def save_to_excel(filepath, name, gender, age, drugs, date_str, price):
     print_sheet['F13'] = age
 
     print_sheet['E15'] = date_str
-    print_sheet['A15'] = price  # 更新总计价格
+    print_sheet['B15'] = price  # 更新总计价格
 
-    for i in range(4):
-        details = ' ;; '.join([f"{drug}" for drug in drugs[i] if drug])
-        if i == 0:
-            print_sheet['A5'] = details
-        elif i == 1:
-            print_sheet['A8'] = details
-        elif i == 2:
-            print_sheet['A11'] = details
-        elif i == 3:
-            print_sheet['A14'] = details
+    # 计算总行高
+    total_height = 0
+    for row in range(5, 16, 3):
+        if print_sheet.row_dimensions[row].height is not None:
+            total_height += print_sheet.row_dimensions[row].height
+        else:
+            total_height += 15  # 默认行高为15
+    # 初始化药品数量列表
+    drug_counts = []
+
+    # 遍历每个药品列表
+    for drug_list in drugs:
+        # 过滤掉空字符串，计算药品数量
+        valid_drug_count = len([drug for drug in drug_list if drug])
+        # 确保药品数量至少为1
+        drug_count = valid_drug_count == 0 and 1 or valid_drug_count * 2
+        # 将计算结果添加到药品数量列表中
+        drug_counts.append(drug_count)
+
+    # 计算药品总数量
+    total_drug_count = sum(drug_counts)
+
+    # 动态调整行高
+    for i, count in enumerate(drug_counts):
+        row = 5 + i * 3
+        print_sheet.row_dimensions[row].height = total_height * (count / total_drug_count)
+
+    # 填充药品详情
+    for i, drug_list in enumerate(drugs):
+        details = '\n'.join([f'{drug}.\t\tqdivgtt' for drug in drug_list if drug])  # 使用适当的空格替代\t
+        cell = f'A{5 + i * 3}'
+        print_sheet[cell] = details
 
     # 更新数据页
     data_sheet = workbook['数据页']
@@ -57,15 +79,14 @@ def save_to_excel(filepath, name, gender, age, drugs, date_str, price):
     data_sheet['B5'] = '总计价格'
     data_sheet['C5'] = price
 
-    for i in range(4):
+    for i, drug_list in enumerate(drugs):
         col_start = 2 + i * 3  # 每联开始的列，间隔3列
         data_sheet.cell(row=6, column=col_start).value = f'第{i + 1}联'
-        for j in range(len(drugs[i])):
-            data_sheet.cell(row=7 + j, column=col_start).value = drugs[i][j]
+        for j, drug in enumerate(drug_list):
+            data_sheet.cell(row=7 + j, column=col_start).value = drug
 
     workbook.save(filepath)
     workbook.close()
-
 
 def load_from_excel(filepath):
     workbook = openpyxl.load_workbook(filepath)
